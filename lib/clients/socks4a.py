122020e87ff57e29361ae0000c439dc04bc962b2
frank2 <frank2@dc949.org>
2016-07-28 19:33:39 -0700

    doing super backwards

--- lib/clients/socks4a.py
+++ lib/clients/socks4a.py
@@ -14,7 +14,7 @@ class Socks4aClient(socks4.Socks4Client):
     REQUEST_PACKET = socks4a.Socks4aRequest
 
     def build_request(self, target_address, port):
-        request = super(self, Socks4aClient).build_request('0.0.0.1', port)
+        request = super(Socks4aClient, self).build_request('0.0.0.1', port)
 
         request.domain.set_value(target_address)
 
